54c3a08b499a61e3a397f2d709fce732223d10c6
Mark Strickland <mstrickland@globys.com>
2019-04-26 08:28:16 -0700

    Setup initial project

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketForm from './NewTicketForm';
+import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketForm} />
+        <Route component={Error404} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
